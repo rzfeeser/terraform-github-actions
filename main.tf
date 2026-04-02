a57124b281062d4fb81dfbe2c0966dde1347a30e
RZFeeser <rzfeeser@users.noreply.github.com>
2026-04-02 16:52:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
+  required_version = ">= 1.1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,7 +11,6 @@ terraform {
       version = "3.4.3"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
@@ -34,70 +32,112 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical2
+  owners = ["099720109477"]
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block           = "172.16.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "tf-example"
+    Name = "tf-example-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "tf-example-igw"
   }
 }
 
 resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "172.16.10.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "tf-example"
+    Name = "tf-example-subnet"
   }
 }
 
-resource "aws_network_interface" "example" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   tags = {
-    Name = "primary_network_interface"
+    Name = "tf-example-public-rt"
   }
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-  primary_network_interface {
-    network_interface_id = aws_network_interface.example.id
-  }
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+resource "aws_security_group" "web_sg" {
+  name   = "${random_pet.sg.id}-sg"
+  vpc_id = aws_vpc.my_vpc.id
+
   ingress {
+    description = "HTTP on 8080"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "tf-example-web-sg"
+  }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.my_subnet.id
+  private_ip                  = "172.16.10.100"
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y apache2
+              sed -i 's/80/8080/g' /etc/apache2/ports.conf
+              sed -i 's/:80>/:8080>/g' /etc/apache2/sites-available/000-default.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl enable apache2
+              systemctl restart apache2
+              EOF
+
+  tags = {
+    Name = "tf-example-web"
+  }
+}
+
+output "web_address" {
+  value = "http://${aws_instance.web.public_dns}:8080"
 }

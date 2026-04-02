d62f0abc969d4108dbb7c1e77a884bd09d9b9fa0
RZFeeser <rzfeeser@users.noreply.github.com>
2026-04-02 16:46:25 -0400

    Create VPC, subnet, and network interface resources
    Added resources for VPC, subnet, and network interface.

--- main.tf
+++ main.tf
@@ -37,11 +37,40 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical2
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "example" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-
+  primary_network_interface {
+    network_interface_id = aws_network_interface.example.id
+  }
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
